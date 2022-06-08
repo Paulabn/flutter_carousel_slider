89629d470c385aa545d935966d5dd6ce09ccb338
Paulabn <paul.abin@gmail.com>
2022-06-08 18:35:14 +0300

    Fixed null safety

--- lib/components/scrollable.dart
+++ lib/components/scrollable.dart
@@ -433,7 +433,7 @@ class ScrollableModifiedState extends State<ScrollableModified>
     _persistedScrollOffset.value = offset;
     // [saveOffset] is called after a scrolling ends and it is usually not
     // followed by a frame. Therefore, manually flush restoration data.
-    ServicesBinding.instance!.restorationManager.flushData();
+    ServicesBinding.instance.restorationManager.flushData();
   }
 
   @override
@@ -708,7 +708,7 @@ class ScrollableModifiedState extends State<ScrollableModified>
           _targetScrollOffsetForPointerScroll(delta);
       // Only express interest in the event if it would actually result in a scroll.
       if (delta != 0.0 && targetScrollOffset != position.pixels) {
-        GestureBinding.instance!.pointerSignalResolver
+        GestureBinding.instance.pointerSignalResolver
             .register(event, _handlePointerScroll);
       }
     }
